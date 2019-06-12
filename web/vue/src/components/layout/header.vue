3ca1b311d63b9b9a1053bde589b0b99712b898b6
chengjie.hu@okcoin.com <chengjie.hu@okcoin.com>
2019-06-12 19:09:23 +0800

    init

---
+++ web/vue/src/components/layout/header.vue
@@ -0,0 +1,46 @@
+<template lang='pug'>
+  div
+    #top
+    header.bg--off-white.grd
+      .contain.grd-row
+        h3.py1.px2.col-2 比特币量化平台（基于Gekko）
+    nav.bg--light-gray
+      .menu.contain
+        router-link(to='/home').py1 介绍
+        router-link(to='/live-gekkos').py1 实盘
+        router-link(to='/backtest').py1 回测
+        router-link(to='/data').py1 数据
+        router-link(to='/config').py1 配置
+        a(href='https://gekko.wizb.it/docs/introduction/about_gekko.html', target='_blank').py1 Gekko文档
+
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.menu {
+  display: flex;
+  flex-direction: row;
+  margin-top: 0;
+  margin-bottom: 2rem;
+}
+
+.menu a {
+  flex: 1 1 100%;
+  display: block;
+  text-align: center;
+  text-decoration: none;
+  color: inherit;
+}
+
+.menu .router-link-active {
+  background-color: rgba(250,250,250,.99);
+}
+
+.menu a:hover {
+  text-decoration: underline;
+}
+
+</style>
---
+++ web/vue/src/components/layout/home.vue
@@ -0,0 +1,31 @@
+<template lang='pug'>
+  section.contain.grd-row
+    .grd-row-col-3-6(v-html='left')
+    .grd-row-col-3-6.txt--center
+      img(src='static/gekko.jpeg')
+      p
+        em The most valuable commodity I know of is information.
+</template>
+
+<script>
+import marked from '../../tools/marked';
+
+const left = marked(`
+
+## BiQuant
+
+BiQuant is a Bitcoin trading bot and backtesting platform that
+connects to popular Bitcoin exchanges. It is written in javascript
+and runs on nodejs.
+
+
+`);
+
+export default {
+  data: () => {
+    return {
+      left
+    }
+  }
+}
+</script>
